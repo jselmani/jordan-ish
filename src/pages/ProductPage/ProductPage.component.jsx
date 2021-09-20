61c6e30168b1c4e5cf4844a3d3913f6dd506b05b
Jiel Selmani <jiel.selmani@gmail.com>
2021-09-20 14:51:55 -0400

    Fixed front end issues that caused oversized images on mobile and tablet views

---
+++ src/pages/ProductPage/ProductPage.component.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+import './ProductPage.styles.scss';
+
+const ProductPage = () => {
+    return (
+        <div className="product-page">
+            Hello
+        </div>
+    );
+};
+
+export default ProductPage;
