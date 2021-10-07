6473edc53049278debf45759277465a9801979bd
Jiel Selmani <jiel.selmani@gmail.com>
2021-10-06 21:28:46 -0400

    Added user redux logic to actions, types, selectors, and began the reducer

--- src/redux/rootReducer.js
+++ src/redux/rootReducer.js
@@ -3,11 +3,13 @@ import { combineReducers } from "redux";
 import directoryReducer from "./directory/directory.reducer";
 import shopReducer from "./shop/shop.reducer";
 import cartReducer from "./cart/cart.reducer";
+import userReducer from "./user/user.reducer";
 
 const rootReducer = combineReducers({
   directory: directoryReducer,
   shop: shopReducer,
   cart: cartReducer,
+  user: userReducer,
 });
 
 export default rootReducer;
