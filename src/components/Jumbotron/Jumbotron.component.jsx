1dfe62bc7a3a1e1d28125d39d3e8b3853a2d8453
Jiel Selmani <jiel.selmani@gmail.com>
2021-11-04 13:38:24 -0400

    Added text to the Jumbotron component so its not just an image

--- src/components/Jumbotron/Jumbotron.component.jsx
+++ src/components/Jumbotron/Jumbotron.component.jsx
@@ -5,6 +5,7 @@ import "./Jumbotron.styles.scss";
 const Jumbotron = () => {
   return (
     <div className="jumbotron">
+      <h1 className="hero-text">BECOME YOUR AIRNESS</h1>
       <img
         className="hero-image"
         src="https://sneakernews.com/wp-content/uploads/2019/09/air-jordan-34-zion-williamson-1.jpg"
